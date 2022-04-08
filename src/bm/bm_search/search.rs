f9af4a45edac7023c458a91719396e446827cd63
doruk <dsekercioglu2003@gmail.com>
2022-04-08 20:43:45 +0200

    Don't include eval in SEE pruning

--- src/bm/bm_search/search.rs
+++ src/bm/bm_search/search.rs
@@ -397,7 +397,7 @@ pub fn search<Search: SearchType>(
         we assume it's safe to prune this move
         */
         let do_see_prune = !Search::PV && moves_seen > 0 && !in_check && depth <= 7;
-        if do_see_prune && eval + see::<16>(pos.board(), make_move) + see_fp(depth) <= alpha {
+        if do_see_prune && see::<16>(pos.board(), make_move) + see_fp(depth) < 0 {
             continue;
         }
 
